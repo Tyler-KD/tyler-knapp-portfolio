423e41a22b98c1209d9d138c910ed3ddd03f6622
unknown <tyler.kd.knapp@gmail.com>
2024-04-11 14:35:05 -0500

    Added tailwind CSS as a PostCSS plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './styles/tailwind.css'
+import About from './pages/AboutPage.jsx'
+import PorfolioPage from './pages/PortfolioPage.jsx'
+import ContactPage from './pages/ContactPage.jsx'
+import ResumePage from './pages/ResumePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+      index: true,
+      element: <About/>,
+      },
+      {
+        path: '/portfolio',
+        element: <PorfolioPage/>
+      },
+      {
+        path: '/contact',
+        element: <ContactPage/>
+      },
+      {
+        path: '/resume',
+        element: <ResumePage/>
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
